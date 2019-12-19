0dfa01e349227a1c757c583991796c99340b5967
mjbaucas <mjbaucas@gmail.com>
2019-12-19 13:39:55 -0500

    Changed client code for testing

--- client.py
+++ client.py
@@ -2,10 +2,25 @@ import requests
 import json
 import time
 
-url = ' http://localhost:3000/send'
+url = ' http://10.11.252.136:3000/send'
 url2 = 'https://8db81646-9af2-4595-ba2f-424c1433fdd3.mock.pstmn.io/send'
 data = {'ID': 10, 'timestamp': int(round(time.time() * 1000))}
 
-x = requests.post(url, json=json.dumps(data), headers={'Content-Type': 'application/json', 'X-Api-Key' : ''})
-print(x.text)
-print(x.elapsed.total_seconds())
+for x in range(0,1000):
+	x = requests.post(url, json=json.dumps(data), headers={'Content-Type': 'application/json', 'X-Api-Key' : ''})
+	elapsed = x.elapsed.total_seconds()
+	print(x.text)
+	print(elapsed)
+
+	f = open('1DeviceLocal.txt', 'a')
+	f.write(str(elapsed) + '\n')
+	f.close()
+	
+	x = requests.post(url2, json=json.dumps(data), headers={'Content-Type': 'application/json', 'X-Api-Key' : ''})
+	elapsed = x.elapsed.total_seconds()
+	print(x.text)
+	print(elapsed)
+
+	f = open('1DevicePostman.txt', 'a')
+	f.write(str(elapsed) + '\n')
+	f.close()
